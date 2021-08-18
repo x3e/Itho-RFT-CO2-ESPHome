1e5dcc2cc6c850b42dadb7d00381c992993076db
Marijn Romeijn <m.j.romeijn@gmail.com>
2021-08-18 17:55:31 +0200

    use template switches instead of custom switch component

--- ithofanComponent/IthoFanComponent.h
+++ ithofanComponent/IthoFanComponent.h
@@ -18,7 +18,12 @@ class IthoFan : public Component {
         void setup() override {
             fanPtr = this;
             fanControl.init();
-            fanControl.addChangedCallback([=](FanStatus newStatus, uint8_t timer){
+            fanControl.addChangedCallback([=](FanStatus newStatus, uint8_t timer) {
+                id(switch_auto).publish_state(newStatus == FanStatus::automatic);
+                id(switch_night).publish_state(newStatus == FanStatus::night);
+                id(switch_low).publish_state(newStatus == FanStatus::low);
+                id(switch_medium).publish_state(newStatus == FanStatus::medium);
+                id(switch_high).publish_state(newStatus == FanStatus::high);
                 id(timer_number).publish_state(timer);
             });
         }
@@ -36,30 +41,4 @@ class IthoFan : public Component {
         }
     private:
         IthoController fanControl;
-};
-
-class IthoSwitch : public Component, public Switch {
-    public:
-        IthoSwitch(FanStatus status) : status(status) {}
-
-        float get_setup_priority() const override {
-            return esphome::setup_priority::AFTER_CONNECTION;
-        }
-
-        void setup() override {
-            fanPtr->registerCallback([=](FanStatus newStatus, uint8_t timer){
-                fanStatusChanged(newStatus);
-            });
-        }
-
-        void write_state(bool state) override {
-            if (state)
-                fanPtr->setStatus(status);
-        }
-
-        void fanStatusChanged(FanStatus newStatus) {
-            publish_state(newStatus == status);
-        }
-    private:
-        FanStatus status;
 };
